3363352b478d061c666b8e4c0e9db28ddedce631
Elon Aseneka <elonsantos63@gmail.com>
2019-09-30 08:32:18 +0300

    feat(core): Add home route

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.json("This is my home page")
+  res.status(200).json("This is my home page")
 });
 
 module.exports = router;
